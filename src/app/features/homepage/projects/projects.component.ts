3a973f8bf3e5a8e565a1d03a539d5eaa5d147b75
Nomera67 <y.rechtenstein@gmail.com>
2023-03-30 18:27:37 +0200

    final version of website

--- src/app/features/homepage/projects/projects.component.ts
+++ src/app/features/homepage/projects/projects.component.ts
@@ -13,8 +13,8 @@ export class ProjectsComponent {
       description: 'Le portfolio sur lequel vous êtes et qui me permet de mettre en avant mes différents projets passés et à venir',
       picture: '../../../../assets/pictures-portfolio/Portfolio.PNG',
       technologies: 'Angular, TypeScript, JavaScript, SCSS, Github, Flexbox, Grid',
-      github: '#',
-      directory: 'http://localhost:4200/home',
+      github: 'https://github.com/Nomera67/portfolio',
+      directory: 'https://www.yrdev.fr',
     },
     {
       name: 'Intégration Finsweet',
