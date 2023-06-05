e99ae11c1735b131cb7a0ab00ada2071dbef0597
Nomera67 <y.rechtenstein@gmail.com>
2023-06-05 23:53:38 +0200

    Beginning of intersectionObserver in resume part

--- src/app/features/about/resume/resume.component.ts
+++ src/app/features/about/resume/resume.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent {
+  // const itemsResume = document.querySelectorAll('resume__item');
+  // const divisionBar = document.querySelectorAll('resume__bar');
+  // if (itemsResume && divisionBar) {
+  //   const observer = new IntersectionObserver(entries => {
+  //     entries.forEach(entry => {
+  //       if (entry.isIntersecting) {
+  //         setTimeout(() => {
+  //           entry.target.classList.add('is-visible');
+  //         }, 200);
+  //       } else {
+  //         setTimeout(() => {
+  //           entry.target.classList.remove('is-visible');
+  //         }, 200);
+  //       }
+  //     });
+  //   });
+  //   observer.observe(itemsResume);
+  //   observer.observe(divisionBar);
+
+
+  // }
 
 }
